fd74fbd198d3ea1df9690621604d3e966ee1fa6c
Kazuya Hatano <k28@me.com>
2020-08-22 06:58:30 +0900

    Part 1

--- Engine/Renderer.swift
+++ Engine/Renderer.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+public struct Renderer {
+    public private(set) var bitmap: Bitmap
+    
+    public init(width: Int, height: Int) {
+        self.bitmap = Bitmap(width: width, height: height, color: .white)
+    }
+}
+
+public extension Renderer {
+    mutating func draw(_ world: World) {
+        // calculate relative scale between world units and pixcels
+        let scale = Double(bitmap.height) / world.size.y
+        
+        // Draw player
+        var rect = world.player.rect
+        rect.min *= scale
+        rect.max *= scale
+        bitmap.fill(rect: rect, color: .blue)
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # RetroRampage-Trace
 
 [RetroRampage](https://github.com/nicklockwood/RetroRampage)をトレースして作成していきます。
+
+
+# タスクリスト
+- [Part1](https://github.com/nicklockwood/RetroRampage/blob/master/Tutorial/Part1.md)
+  - __here__ Invension of Control
+
+<!-- vim:set ft=markdown ts=2 sw=2 sts=2: -->
